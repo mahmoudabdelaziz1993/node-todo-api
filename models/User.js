60eb62bac3b4ed9aeb9d09e5ed0d0a8eda06e79c
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2018-11-29 00:39:36 +0200

    sitting auth with JWT and authorized middelware

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+const bycrpt = require('bcrypt-nodejs');
 
 
 var UserSchema = new mongoose.Schema({
@@ -8,8 +10,38 @@ var UserSchema = new mongoose.Schema({
 		minlength:1,
 		trim:true,
 		unique: true
+	},
+	password:{
+		type: String,
+		required: true,
+		minlength:1,
+		trim:true,
+	},
+	token:{
+		type: String,
+		
 	}
 });
+
+//---------------------------------- genrate the token  ---------------------------
+UserSchema.methods.genratetoken=function(){
+	var token = jwt.sign({ id:this._id.toHexString() }, 'secretxxx').toString();
+	return token ;
+};
+
+
+//----------------------------------- verify the token ---------------------------
+
+//-----------------------------------bycrpt password ---------------------------
+UserSchema.methods.encryptpass = function (password) {
+	return bycrpt.hashSync(password,bycrpt.genSaltSync(10),null);
+}
+
+//--------------------------------- compare password with the hash ----------------
+UserSchema.methods.validpassword = function(password){
+	return bycrpt.compareSync(password, this.password);
+
+}
 
 
 var User = mongoose.model('users',UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var {User} = require('../models/User');
+var {mongoose} = require('../config/db');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  var body = _.pick(req.body,['email','password']);
+  var newuser = new User();
+  newuser.email = body.email;
+  newuser.password = newuser.encryptpass(body.password);
+ // newuser.token = newuser.genratetoken();
+  newuser.save((err,user)=>{
+  	if (err) {
+  		res.status(404).send(err);
+  	}
+  	if (user) {
+  		user.token =newuser.genratetoken();
+  		user.save((err,user)=>{
+  			if (err) {res.status(404).send(err);}
+  			if (user) {res.header('x-auth',user.token).status(200).send({token:user.token});}
+  		});
+  		
+  	}
+  })
+});
+
+router.get('/',authorized, function(req, res, next) {
+  res.send(req.user)
 });
 
 module.exports = router;
+
+function authorized(req,res,next) {
+	var token = req.header('x-auth');
+	var decoded;
+	if (_.isEmpty(token)) {
+		res.status(404).send({"message":" not authorized "});
+	}else{
+
+		try{
+			var decoded = jwt.verify(token, 'secretxxx');
+
+		}catch(e){
+			res.status(404).send({"message":" invalid token dsadsa "});
+		}
+		User.findOne({ token:token,_id:decoded.id }, function (err, user) {
+			if (err) {
+				res.status(404).send({"message":" invalid token ...... "});
+			}
+			if (user) {
+				req.user = user;
+				req.token = token;
+				next();
+			}
+		});
+
+
+	}
+};
